d071f390c5497ba40a4ec83c68b809307b6a5cfe
1user-name1 <119626792+1user-name1@users.noreply.github.com>
2023-03-24 06:27:25 +0500

    Update first_task.h

--- template_comparision_task/first_task.h
+++ template_comparision_task/first_task.h
@@ -1,4 +1,43 @@
-class NewA
+#include <string_view>
+#include <string>
+
+template<typename T1>
+class OtherComparisionOperators
+{
+public:
+
+    template <typename T2>
+    bool operator<=(const T2& other_object) const
+    {
+        return !(this_object() > other_object);
+    }
+
+    template <typename T2>
+    bool operator>=(const T2& other_object) const
+    {
+        return !(this_object() < other_object);
+    }
+
+    template <typename T2>
+    bool operator==(const T2& other_object) const
+    {
+        return !(this_object() < other_object) && !(this_object() > other_object);
+    }
+
+    template <typename T2>
+    bool operator!=(const T2& other_object) const
+    {
+        return this_object() > other_object || this_object() < other_object;
+    }
+private:
+    const T1& this_object() const
+    {
+        return *static_cast<const T1*>(this);
+    }
+
+};
+
+class NewA : public OtherComparisionOperators<NewA>
 {
 public:
     NewA(int a, int b) : m_a(a), m_b(b){}
@@ -25,7 +64,60 @@ public:
         return m_a > other;
     }
 
+    // Операторы сравнения с char
+    bool operator<(const char other) const
+    {
+        return m_a < static_cast<int>(other);
+    }
+
+    bool operator>(const char other) const
+    {
+        return m_a > static_cast<int>(other);
+    }
+
+    // Операторы сравнения с double
+    bool operator<(const double other) const
+    {
+        return m_a < static_cast<int>(other);
+    }
+
+    bool operator>(const double other) const
+    {
+        return m_a > static_cast<int>(other);
+    }
+
+    // Операторы сравнения с float
+    bool operator<(const float other) const
+    {
+        return m_a < static_cast<int>(other);
+    }
+
+    bool operator>(const float other) const
+    {
+        return m_a > static_cast<int>(other);
+    }
+
 private:
     int m_a = 0;
     int m_b = 0;
+};
+
+class NewB : public OtherComparisionOperators<NewB>
+{
+public:
+    NewB(std::string_view val) : m_stringView(val) {}
+
+    // Операторы сравнения с std::string
+    bool operator<(const std::string& other) const
+    {
+        return m_stringView < other;
+    }
+
+    bool operator>(const std::string& other) const
+    {
+        return m_stringView > other;
+    }
+
+private:
+    const std::string_view m_stringView;
 };
